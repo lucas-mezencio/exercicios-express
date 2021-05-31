7ab73767eab534a3b2bd589fd33d1961bb2f3a3b
Lucas Mezencio <lucasmezencio@outlook.com>
2021-05-30 21:43:05 -0300

    middlewares

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get('/ola', (req, res) => {
-    res.send('Estou funcionando!')
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('Lucas'))
+
+app.use('/ola', (req, res, next) => {
+    console.log('antes...');
+    next()
+})
+
+app.get('/ola', (req, res, next) => {
+    console.log('durante...');
+    res.json({
+        name: 'ipad',
+        price: 1899.00,
+        discount: 0.10
+    })
+    next()
+})
+
+app.use('/ola', (req, res) => {
+    console.log('depois...')
 })
 
 app.listen(3000, () => {
