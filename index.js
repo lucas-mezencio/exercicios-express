353ad44d5f8bd4c3654cd1f9597bc00e0a10e441
Lucas Mezencio <lucasmezencio@outlook.com>
2021-05-30 21:00:19 -0300

    tratando primeira requisicao

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/ola', (req, res) => {
+    res.send('Estou funcionando!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...');
 })
